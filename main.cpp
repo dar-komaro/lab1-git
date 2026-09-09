a3221c4fc0473dae3d133ea4525301d8f2308830
Darya Komarovskaya <komarod07@gmail.com>
2026-09-09 19:40:18 +0700

    Добавлена вторая расчетная функция и пункт меню

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // Лабораторная работа № 1. Вариант N 27.
 // Выполнил: Комаровская Д. Е., группа ПИ-52.
 #include <iostream>
+#include <clocale>
 using namespace std;
 
 // ===== Расчётные функции варианта =====
@@ -10,16 +11,23 @@ double density(double m, double V) {
 	return m / V;
 }
 
+//Масса
+double massFromDensity(double rho, double V) {
+	return rho * V;
+}
+
 
 // ===== Главная функция: меню =====
 
 int main() {
+	setlocale(LC_ALL, "Russ");
 	int choice;
-	double m, V;
+	double m, rho, V;
 
 	do {
 		cout << "\n=== Вариант N27: расчёты для плотности===\n";
 		cout << "1. Плотность\n";
+		cout << "2.Масса\n";
 		cout << "0. Выход\n";
 		cout << "Выберите пункт: ";
 		cin >> choice;
@@ -29,6 +37,11 @@ int main() {
 			cout << "Введите массу m и объем V: ";
 			cin >> m >> V;
 			cout << "Плотность = " << density(m, V) << "\n";
+			break;
+		case 2:
+			cout << "Введите плотность rho и объем V: ";
+			cin >> rho >> V;
+			cout << "Масса = " << massFromDensity(rho, V) << "\n";
 			break;
 		case 0:
 			cout << "Работа завершена.\n";
