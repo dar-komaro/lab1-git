96602e0b672752c15a9b65a71e6a7c73c7fa957c
Darya Komarovskaya <komarod07@gmail.com>
2026-09-09 20:16:34 +0700

    Добавлена третья функция варианта

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 // Лабораторная работа № 1. Вариант N 27.
 // Выполнил: Комаровская Д. Е., группа ПИ-52.
 #include <iostream>
-#include <clocale>
+#include <windows.h> 
 using namespace std;
 
 // ===== Расчётные функции варианта =====
@@ -16,11 +16,17 @@ double massFromDensity(double rho, double V) {
 	return rho * V;
 }
 
+//Объем 
+double volumeFromDensity(double m, double rho) {
+	return m / rho;
+}
+
 
 // ===== Главная функция: меню =====
 
 int main() {
-	setlocale(LC_ALL, "Russ");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	int choice;
 	double m, rho, V;
 
@@ -28,6 +34,7 @@ int main() {
 		cout << "\n=== Вариант N27: расчёты для плотности===\n";
 		cout << "1. Плотность\n";
 		cout << "2.Масса\n";
+		cout << "3.Объем\n";
 		cout << "0. Выход\n";
 		cout << "Выберите пункт: ";
 		cin >> choice;
@@ -42,6 +49,11 @@ int main() {
 			cout << "Введите плотность rho и объем V: ";
 			cin >> rho >> V;
 			cout << "Масса = " << massFromDensity(rho, V) << "\n";
+			break;
+		case 3:
+			cout << "Введите массу m и плотность rho: ";
+			cin >> m >> rho;
+			cout << "Объем = " << volumeFromDensity(m,rho) << "\n";
 			break;
 		case 0:
 			cout << "Работа завершена.\n";
